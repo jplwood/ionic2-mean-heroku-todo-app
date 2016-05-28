727fb633cbf9e4a38652d07e360d79080352f72d
Justin <jplwood@gmail.com>
2016-05-28 18:03:11 -0500

    Adding whole app to repo

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ var mongodb = require('mongodb'),
 app.use(bodyParser.json());
 app.set('port', process.env.PORT || 8080);
 app.use(cors()); // CORS (Cross-Origin Resource Sharing) headers to support Cross-site HTTP requests
-app.use(express.static(__dirname + "/www")); // Our Ionic app build (kept up-to-date by the Ionic CLI using 'ionic serve')
+app.use(express.static("www")); // Our Ionic app build (kept up-to-date by the Ionic CLI using 'ionic serve')
 
 
 var MONGODB_URI = process.env.MONGODB_URI || 'mongodb://heroku_g24xsxd8:ur8k708rh1qqgr17luq6satqct@ds045622.mlab.com:45622/heroku_g24xsxd8'
