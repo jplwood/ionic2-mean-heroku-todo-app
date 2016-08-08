66d006b8fd5425d67de1669851a558f009fa4403
Justin Leatherwood <jplwood@gmail.com>
2016-08-08 18:25:05 -0500

    added rxjs import to todoservice to fix broken functionality

--- app/providers/todo-service/todo-service.ts
+++ app/providers/todo-service/todo-service.ts
@@ -1,6 +1,7 @@
 import {Injectable} from '@angular/core';
 import {Http, Headers} from '@angular/http';
 import {Observable} from 'rxjs/Observable';
+import 'rxjs/Rx';
 import {Todo} from '../../todo.ts';
 
 
